b49a33b9b2114f4aa26a7ed63af37c09823d7462
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-23 20:15:22 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.warn = () => {};
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
