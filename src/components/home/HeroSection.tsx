ccd12858204856cfc9a72a4baae5848d2c898836
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-25 15:54:40 +0530

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -135,6 +135,7 @@ const HeroSection: React.FC = () => {
                 className="btn-outline-gold w-full sm:w-auto py-4 sm:py-3"
               >
                 Explore Menu
+               <ArrowRight className="inline-block ml-2 w-4 h-4 group-hover:translate-x-1 transition-transform" />
               </motion.button>
             </Link>
           </motion.div>
