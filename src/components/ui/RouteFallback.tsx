7add4477152945e5a8b7d1cedd9cb7ee02d98cf5
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-25 12:38:48 +0530

    Create RouteFallback.tsx

---
+++ src/components/ui/RouteFallback.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RouteFallback = () => {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      {/* intentional empty fallback to avoid blank screen */}
+    </div>
+  );
+};
+
+export default RouteFallback;
