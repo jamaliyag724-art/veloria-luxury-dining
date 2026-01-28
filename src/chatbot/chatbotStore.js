83e4f93b7800cef78b07f2ee1c3e4aabc4c57f81
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-28 15:52:04 +0530

    Update chatbotStore.js

--- src/chatbot/chatbotStore.js
+++ src/chatbot/chatbotStore.js
@@ -2,8 +2,14 @@ import { create } from "zustand";
 
 export const useChatbotStore = create((set) => ({
   isOpen: false,
+  messages: [
+    {
+      from: "bot",
+      text: "Welcome to Veloria. How may I assist you today?"
+    }
+  ],
 
   openChat: () => set({ isOpen: true }),
   closeChat: () => set({ isOpen: false }),
-  toggleChat: () => set((s) => ({ isOpen: !s.isOpen })),
+  toggleChat: () => set((s) => ({ isOpen: !s.isOpen }))
 }));
