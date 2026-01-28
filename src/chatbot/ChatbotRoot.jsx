fb254a6c974d1212cb57cc2b4d4fc6c3d19c7e45
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-28 15:53:20 +0530

    Update ChatbotRoot.jsx

--- src/chatbot/ChatbotRoot.jsx
+++ src/chatbot/ChatbotRoot.jsx
@@ -1,22 +1,11 @@
-import { createPortal } from "react-dom";
-import { useEffect, useState } from "react";
 import ChatbotButton from "./ChatbotButton";
 import ChatbotPanel from "./ChatbotPanel";
 
 export default function ChatbotRoot() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
-  return createPortal(
+  return (
     <>
       <ChatbotButton />
       <ChatbotPanel />
-    </>,
-    document.body
+    </>
   );
 }
