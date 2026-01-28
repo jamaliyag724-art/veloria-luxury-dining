01095ae0b8ac3205ff4c0430ab0cf08705086bf3
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-28 16:05:59 +0530

    Create reservationData.js

---
+++ src/chatbot/reservationData.js
@@ -0,0 +1,16 @@
+export const reservations = {
+  "RV-1024": {
+    name: "Gaurang",
+    guests: 2,
+    date: "29 January",
+    time: "8:00 PM",
+    status: "Confirmed"
+  },
+  "RV-2048": {
+    name: "Ananya",
+    guests: 4,
+    date: "30 January",
+    time: "9:30 PM",
+    status: "Pending"
+  }
+};
