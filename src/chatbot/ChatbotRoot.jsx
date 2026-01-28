d757e300d7f5cbd3ee51719e238960fbe7a19921
jamaliyag724-art <jamaliyag724@gmail.com>
2026-01-28 19:11:44 +0530

    Update ChatbotRoot.jsx

--- src/chatbot/ChatbotRoot.jsx
+++ src/chatbot/ChatbotRoot.jsx
@@ -1,12 +1,11 @@
 import ChatbotButton from "./ChatbotButton";
 import ChatbotPanel from "./ChatbotPanel";
-import ChatbotPortal from "./ChatbotPortal";
 
 export default function ChatbotRoot() {
   return (
-    <ChatbotPortal>
+    <>
       <ChatbotButton />
       <ChatbotPanel />
-    </ChatbotPortal>
+    </>
   );
 }
