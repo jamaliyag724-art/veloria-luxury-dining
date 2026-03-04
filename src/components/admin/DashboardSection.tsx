933c5ace397a897b5d57320c537fdecbe730ae9e
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 19:31:19 +0530

    Update DashboardSection.tsx

--- src/components/admin/DashboardSection.tsx
+++ src/components/admin/DashboardSection.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import RevenueCard from "./RevenueCard";
 
 const DashboardSection = () => {
+
   return (
-    <div className="text-3xl">
-      Dashboard Working
+
+    <div className="space-y-10">
+
+      <div className="grid md:grid-cols-4 gap-6">
+
+        <RevenueCard title="Revenue" value={5400}/>
+        <RevenueCard title="Orders" value={27}/>
+        <RevenueCard title="Avg Order" value={203}/>
+        <RevenueCard title="Customers" value={27}/>
+
+      </div>
+
     </div>
+
   );
+
 };
 
 export default DashboardSection;
