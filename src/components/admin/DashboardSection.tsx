771e00e5b0728b5e7650c1164dc58cb577861837
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 20:05:34 +0530

    Update DashboardSection.tsx

--- src/components/admin/DashboardSection.tsx
+++ src/components/admin/DashboardSection.tsx
@@ -1,35 +1,61 @@
-import React from "react";
+import React from "react"
 
 import RevenueCard from "./RevenueCard"
 import RevenueChart from "./RevenueChart"
 import OrdersChart from "./OrdersChart"
-import ReservationChart from "./ReservationChart"
 
-import { DollarSign,ShoppingCart,Users,BarChart3 } from "lucide-react"
+import {
+DollarSign,
+ShoppingCart,
+Users,
+BarChart3
+} from "lucide-react"
 
-const DashboardSection = ()=>{
+const DashboardSection=()=>{
 
 return(
 
 <div className="space-y-10">
 
+<h1 className="text-3xl font-semibold">
+Veloria Analytics
+</h1>
+
 <div className="grid md:grid-cols-4 gap-6">
 
-<RevenueCard title="Revenue" value={5400} icon={DollarSign} isCurrency/>
-<RevenueCard title="Orders" value={27} icon={ShoppingCart}/>
-<RevenueCard title="Avg Order" value={203} icon={BarChart3}/>
-<RevenueCard title="Customers" value={27} icon={Users}/>
+<RevenueCard
+title="Revenue"
+value="₹5400"
+icon={DollarSign}
+/>
+
+<RevenueCard
+title="Orders"
+value="27"
+icon={ShoppingCart}
+/>
+
+<RevenueCard
+title="Avg Order"
+value="₹203"
+icon={BarChart3}
+/>
+
+<RevenueCard
+title="Customers"
+value="27"
+icon={Users}
+/>
 
 </div>
 
 <div className="grid lg:grid-cols-2 gap-8">
 
-<RevenueChart orders={[]}/>
-<OrdersChart orders={[]} dateRange="30days"/>
+<RevenueChart/>
+
+<OrdersChart/>
 
 </div>
-
-<ReservationChart/>
 
 </div>
 
