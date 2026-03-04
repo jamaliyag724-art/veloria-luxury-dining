996da9c6d13b56da42e563e48d49f0d11bc33151
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 19:28:56 +0530

    Update DashboardSection.tsx

--- src/components/admin/DashboardSection.tsx
+++ src/components/admin/DashboardSection.tsx
@@ -1,40 +1,11 @@
 import React from "react";
-import RevenueCard from "./RevenueCard";
-import OrdersChart from "./OrdersChart";
-import RevenueChart from "./RevenueChart";
 
 const DashboardSection = () => {
-
-  const orders = [];
-
   return (
-
-    <div className="space-y-10">
-
-      <div className="grid md:grid-cols-4 gap-6">
-
-        <RevenueCard title="Revenue" value={5400}/>
-        <RevenueCard title="Orders" value={27}/>
-        <RevenueCard title="Avg Order" value={203}/>
-        <RevenueCard title="Customers" value={27}/>
-
-      </div>
-
-      <div className="grid lg:grid-cols-2 gap-8">
-
-        <RevenueChart orders={orders} />
-
-        <OrdersChart
-          orders={orders}
-          dateRange="30days"
-        />
-
-      </div>
-
+    <div className="text-3xl">
+      Dashboard Working
     </div>
-
   );
-
 };
 
 export default DashboardSection;
