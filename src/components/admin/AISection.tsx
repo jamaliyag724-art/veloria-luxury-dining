65e7c0fde15e7f8d818021ad4bc953e5b947b756
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 18:13:33 +0530

    Create AISection.tsx

---
+++ src/components/admin/AISection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AIInsights from "../AIInsights";
+
+const AISection = () => {
+
+  return (
+
+    <div>
+
+      <h2 className="text-xl font-serif mb-6">
+        AI Restaurant Insights
+      </h2>
+
+      <AIInsights/>
+
+    </div>
+
+  );
+
+};
+
+export default AISection;
