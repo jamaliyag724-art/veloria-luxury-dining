35701508a124bd19b1f00cbee6bf7b584d079315
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 18:11:53 +0530

    Create AnalyticsSection.tsx

---
+++ src/components/admin/AnalyticsSection.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import TopItemsChart from "../TopItemsChart";
+import OrderStatusChart from "../OrderStatusChart";
+import PeakHoursChart from "../PeakHoursChart";
+
+const AnalyticsSection = () => {
+
+  return (
+
+    <div className="space-y-10">
+
+      <div className="grid lg:grid-cols-2 gap-8">
+
+        <TopItemsChart/>
+
+        <OrderStatusChart/>
+
+      </div>
+
+      <PeakHoursChart/>
+
+    </div>
+
+  );
+
+};
+
+export default AnalyticsSection;
