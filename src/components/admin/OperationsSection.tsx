6b20e3d1311d5cf5e1f7d0fe037dc79859bc33aa
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 18:13:05 +0530

    Create OperationsSection.tsx

---
+++ src/components/admin/OperationsSection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import RecentOrders from "../RecentOrders";
+
+const OperationsSection = () => {
+
+  return (
+
+    <div>
+
+      <h2 className="text-xl font-serif mb-6">
+        Operations
+      </h2>
+
+      <RecentOrders/>
+
+    </div>
+
+  );
+
+};
+
+export default OperationsSection;
