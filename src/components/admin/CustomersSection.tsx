fdb67cd7db3aca0aa3989e4ecbd9c183a12a41bd
jamaliyag724-art <jamaliyag724@gmail.com>
2026-03-04 18:12:30 +0530

    Create CustomersSection.tsx

---
+++ src/components/admin/CustomersSection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TopCustomers from "../TopCustomers";
+
+const CustomersSection = () => {
+
+  return (
+
+    <div>
+
+      <h2 className="text-xl font-serif mb-6">
+        Top Customers
+      </h2>
+
+      <TopCustomers/>
+
+    </div>
+
+  );
+
+};
+
+export default CustomersSection;
